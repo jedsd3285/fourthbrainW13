72397451cdc23a1cf27a13b061ad74dadb59a3cb
Jose Eduardo Pineda Sto. Domingo <jstodomingo@aim.edu>
2022-05-13 13:28:04 +0800

    Using Neha's chosen model

--- main.py
+++ main.py
@@ -5,3 +5,20 @@ app = FastAPI()
 def health():
     return "Service is online."
 
+from transformers import pipeline
+# sentiment_model = pipeline("automatic-speech-recognition", model="facebook/wav2vec2-base-960h")
+sentiment_model = pipeline("sentiment-analysis", model="nlptown/bert-base-multilingual-uncased-sentiment")
+
+
+# sentiment_query_sentence = get_random_comment(top_comments)
+# sentiment = sentiment_model(sentiment_query_sentence)
+# print(f"Sentiment test: {sentiment_query_sentence} == {sentiment})
+
+from pydantic import BaseModel
+
+class PredictionRequest(BaseModel):
+  query_string: str
+
+@app.post("/my-endpoint")
+def my_endpoint(request: PredictionRequest):
+    return sentiment_model(request.query_string)
